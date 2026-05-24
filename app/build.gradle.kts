1a852883592962ccbbf52083d95972fcde81d0ae
ElenaM11 <manamielenaherrera@gmail.com>
2026-05-24 15:04:24 -0400

    Interfaces y configuración de dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
+
+    id("com.google.gms.google-services")
     alias(libs.plugins.android.application)
+
 }
 
 android {
@@ -36,6 +39,17 @@ android {
 }
 
 dependencies {
+    //dependencias utilizadas en el proyecto
+    implementation("com.google.ar:core:1.31.0")
+    implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
+    implementation("com.google.firebase:firebase-auth:21.1.0")
+    implementation("com.google.firebase:firebase-firestore:24.4.0")
+    implementation("org.tensorflow:tensorflow-lite:2.14.0")
+    implementation("com.gorisse.thomas.sceneform:sceneform:1.23.0")
+    //firebase
+    implementation(platform("com.google.firebase:firebase-bom:34.13.0"))
+    implementation("com.google.firebase:firebase-analytics")
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
